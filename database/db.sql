5258af03f9f7de34a116f3cc1ec735bdd563e78e
pabluskagamo <pabluskagamo@gmail.com>
2022-03-18 14:06:00 +0100

    Script sql actualizado

--- database/db.sql
+++ database/db.sql
@@ -1,9 +1,7 @@
-CREATE TABLE personas (
+CREATE TABLE usuario (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
-  apellido VARCHAR(100) NOT NULL
+  correo VARCHAR(100) NOT NULL,
+  contraseya VARCHAR(50) NOT NULL
 );
-
-insert into personas(nombre,apellido)
-values('asd','asddsa');
 
