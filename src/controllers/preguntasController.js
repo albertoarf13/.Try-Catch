495b0cf8be1fa18c9ec96d0d34331ab920b23922
PabloJimenez98 <53871224+PabloJimenez98@users.noreply.github.com>
2022-03-19 20:05:57 +0100

    ctrl prg

---
+++ src/controllers/preguntasController.js
@@ -0,0 +1,27 @@
+const preguntaController = {};
+//const dbUser = require(../integracion/dbUser)
+
+preguntaController.atribs = (req, res) => {
+    const idPregunta = req.body;
+
+    req.getConnection((err, conn)=>{
+        
+        conn.query("SELECT * FROM preguntas WHERE idpregunta = ?", [idPregunta], (err, infoPregunta)=>{
+
+            if(err){
+                res.json(err);
+            }
+            else if(infoPregunta.length == 0){
+                res.render('atributosPregunta.ejs', { error: "No se ha podido encontrar la pregunta" });
+            }else{
+                res.send(infoPregunta);
+            }
+
+        });
+    });
+}
+
+preguntaController.atribs_page = (req, res) => {
+    
+    res.render('atributosPregunta.ejs');
+}
