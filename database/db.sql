fa80d6417fcb649dee409195ab075c5336756e35
PabloJimenez98 <53871224+PabloJimenez98@users.noreply.github.com>
2022-03-19 19:19:14 +0100

    tablas prg

--- database/db.sql
+++ database/db.sql
@@ -5,3 +5,28 @@ CREATE TABLE usuario (
   contraseya VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE preguntas (
+  idpregunta int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  idusuario int(100) NOT NULL,
+  titulo varchar(200) NOT NULL,
+  cuerpo varchar(2000) NOT NULL,
+  fecha date NOT NULL
+);
+
+CREATE TABLE respuestas (
+  idrespuesta int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  respuesta varchar(1000) NOT NULL,
+  idusuario int(100) NOT NULL,
+  idpregunta int(100) NOT NULL,
+  fecha date NOT NULL
+);
+
+CREATE TABLE tags (
+  idtag int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  texto varchar(20) NOT NULL
+);
+
+CREATE TABLE tagpreg (
+  idpregunta int(100) NOT NULL PRIMARY KEY,
+  idtag int(100) NOT NULL
+)
