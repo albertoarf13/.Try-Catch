7e0013e6f46a827bdd3648efae80017239016052
Dunia30 <100685567+Dunia30@users.noreply.github.com>
2022-03-20 13:44:25 +0100

    Update db.sql
    Tablas pregunta y respuesta creadas

--- database/db.sql
+++ database/db.sql
@@ -5,3 +5,18 @@ CREATE TABLE usuario (
   contraseya VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE pregunta(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(100),
+  descripcion VARCHAR(100),
+  imagen varbinary(8000),
+  correo VARCHAR(100) NOT NULL REFERENCES usuario(correo)  
+);
+
+CREATE TABLE respuesta(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	descripcion VARCHAR(100),
+  imagen varbinary(8000),
+	idPregunta INT(6) NOT NULL REFERENCES pregunta(id),
+  correo VARCHAR(100) NOT NULL REFERENCES usuario(correo) 
+);
