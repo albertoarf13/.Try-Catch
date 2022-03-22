aee484e4807668806aac5e7af913194b2ee8c828
Alberto <rivaalberto@gmail.com>
2022-03-22 20:40:18 +0100

    Cambiar valores de pregunta de test de crear pregunta

--- Test/user.test.js
+++ Test/user.test.js
@@ -81,17 +81,15 @@ test('[Crear pregunta] con sesion iniciada', async () => {
 
     const usuario = { correo: 'alberiva@ucm.es', contraseya: '123'};
     const pregunta = {
-        titulo: "hola",
-        descripcion: "asdad",
+        titulo: "Pregunta de test",
+        descripcion: "test",
         etiquetas: [1,2,3]
     }
 
     let testSession = session(app);
 
 
-    const response = await testSession
-    .post('/login')
-    .send(usuario);
+    const response = await testSession.post('/login').send(usuario);
     
     const response2 = await testSession.post('/preguntas/crear').send(pregunta);
     expect(response2.status).toBe(200);
