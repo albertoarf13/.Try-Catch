5df9ba66eda93342f883c9a436792fa3aec75fe4
Pabluskagamo <pabluskagamo@gmail.com>
2022-03-29 12:25:06 +0200

    kljdkljads

--- test/calculator.test.js
+++
@@ -1,9 +0,0 @@
-const opc = require("./calculator");
-
-test("adds 1 + 2 to equal 3", () =>{
-    expect(opc.suma(1, 2)).toBe(3);
-});
-test("subs 1 - 2 to equal -1", () =>{
-    expect(opc.resta(1, 2)).toBe(-1);
-});
-
