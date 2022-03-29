8a10752e257c9eb730b7384c4a214efda5cf5ad4
Pabluskagamo <pabluskagamo@gmail.com>
2022-03-29 16:33:37 +0200

    min cambios

--- Test/verAtributos.test.js
+++ Test/verAtributos.test.js
@@ -10,14 +10,14 @@ beforeAll(() => {
 
 
 test('[Ver Atributos] con id existente', async () => {
-    const response3 = await request(app).get('/atributoPregunta/1');
+    const response3 = await request(app).get('/preguntas/mostrar/1');
     expect(response3.status).toBe(450);
 
 });
 
 
 test('[Ver Atributos] con id no existente', async () => {
-    const response3 = await request(app).get('/atributoPregunta/-1');
+    const response3 = await request(app).get('/preguntas/mostrar/-1');
     expect(response3.status).toBe(451);
 });
 
