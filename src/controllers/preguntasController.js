553e82dc67c909578a7f1d5111e515ecca85ea89
Alberto <rivaalberto@gmail.com>
2022-04-10 23:23:20 +0200

    Refactorizar la búsqueda básica guardando las queries en variables

--- src/controllers/preguntasController.js
+++ src/controllers/preguntasController.js
@@ -310,6 +310,69 @@ preguntasController.responder_respuesta = (req, res) =>{
 
 }
 
+let query_busqueda_basica = `select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
+    from pregunta
+    left join etiqueta_pregunta
+    on pregunta.id =  etiqueta_pregunta.id_pregunta
+    left join etiqueta
+    on etiqueta_pregunta.id_etiqueta = etiqueta.id
+    where titulo LIKE ?
+    group by pregunta.id
+    order by id desc
+    limit 10 offset ?;`;
+
+let query_busqueda_no_respondidas = `select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
+    from (
+        select pregunta.*, COUNT(respuesta.id) as num_respuestas
+        from pregunta
+        left join respuesta
+        on pregunta.id = respuesta.idPregunta
+        group by pregunta.id
+    ) as pregunta
+    left join etiqueta_pregunta
+    on pregunta.id =  etiqueta_pregunta.id_pregunta
+    left join etiqueta
+    on etiqueta_pregunta.id_etiqueta = etiqueta.id
+    where titulo LIKE ? and pregunta.num_respuestas = 0
+    group by pregunta.id
+    order by id desc
+    limit 10 offset ?;`;
+
+let query_busqueda_respondidas = `select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
+    from (
+        select pregunta.*, COUNT(respuesta.id) as num_respuestas
+        from pregunta
+        left join respuesta
+        on pregunta.id = respuesta.idPregunta
+        group by pregunta.id
+    ) as pregunta
+    left join etiqueta_pregunta
+    on pregunta.id =  etiqueta_pregunta.id_pregunta
+    left join etiqueta
+    on etiqueta_pregunta.id_etiqueta = etiqueta.id
+    where titulo LIKE ? and pregunta.num_respuestas > 0
+    group by pregunta.id
+    order by id desc
+    limit 10 offset ?;`;
+
+let query_busqueda_por_etiquetas = `select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
+    from pregunta
+    left join etiqueta_pregunta
+    on pregunta.id =  etiqueta_pregunta.id_pregunta
+    left join etiqueta
+    on etiqueta_pregunta.id_etiqueta = etiqueta.id
+    where pregunta.id IN (
+        select distinct pregunta.id
+        from etiqueta_pregunta
+        inner join pregunta
+        on etiqueta_pregunta.id_pregunta = pregunta.id
+        where titulo LIKE ? and id_etiqueta in (?)
+    )
+    group by pregunta.id
+    order by pregunta.id desc
+    limit 10 offset ?;`;
+
+
 preguntasController.busqueda_basica = (req, res) => {
     let page = req.query.page;
     let offset;
@@ -328,129 +391,58 @@ preguntasController.busqueda_basica = (req, res) => {
     const info = req.query.bus;
     var dynamicInput = '%'.concat(info.concat('%'));
 
-    let callback = (err, lista_preguntas)=>{
-
-        lista_preguntas.map(pregunta=>{
-            pregunta.etiquetas = pregunta.etiquetas.split(',');
-            return pregunta.etiquetas;
-        })
-        if(err){
-            res.json(err);
-        }
-        else {
-            let currentPage = req.url;
-
-            if(currentPage.indexOf('page=') == -1){
-                currentPage = currentPage + '&';
-            }
-            else{
-                currentPage = currentPage.substring(0, currentPage.indexOf('page='));
-            }
-
-            var preguntas = JSON.parse(JSON.stringify(lista_preguntas));
-            res.status(401).render('busquedaBasica.ejs', {
-                preguntas : preguntas, 
-                currentPage: currentPage, 
-                pag: page,
-                query : req.query
-            });
-        }
+    // Comprobar si hay que hacer una búsqueda por etiquetas
+    let etiquetas = req.query;
+    if(!Array.isArray(etiquetas)){
+        etiquetas = [etiquetas];
     }
 
+    // Por defecto es búsqueda básica
+    let query = query_busqueda_basica;
 
-
-    if(req.query.respondidas == undefined){
-        buscar_basico(req, dynamicInput, offset, callback);
-    }
-    else if(req.query.respondidas == "false"){
-        buscar_no_respondidas(req, dynamicInput, offset, callback);
+    if(req.query.respondidas == "false"){
+        query = query_busqueda_no_respondidas;
     }
     else if(req.query.respondidas == "true"){
-        buscar_respondidas(req, dynamicInput, offset, callback);
+        query = query_busqueda_respondidas;
     }
-    else{
-        buscar_basico(req, dynamicInput, offset, callback);
-    }
+
+
+    req.getConnection((err, conn)=>{
+
+        conn.query(query, [dynamicInput, offset] ,(err, lista_preguntas)=>{
+            lista_preguntas.map(pregunta=>{
+                pregunta.etiquetas = pregunta.etiquetas.split(',');
+                return pregunta.etiquetas;
+            })
+
+            if(err){
+                res.json(err);
+            }
+            else {
+                let currentPage = req.url;
+
+                if(currentPage.indexOf('page=') == -1){
+                    currentPage = currentPage + '&';
+                }
+                else{
+                    currentPage = currentPage.substring(0, currentPage.indexOf('page='));
+                }
+
+                var preguntas = JSON.parse(JSON.stringify(lista_preguntas));
+                res.status(401).render('busquedaBasica.ejs', {
+                    preguntas : preguntas, 
+                    currentPage: currentPage, 
+                    pag: page,
+                    query : req.query
+                });
+            }
+        });
+
+    });
     
 }
 
-function buscar_basico(req, dynamicInput, offset, callback){
-
-    req.getConnection((err, conn)=>{
-        //conn.query("SELECT * FROM preguntas WHERE descripcion LIKE ?", [dynamicInput], (err, lista_preguntas)=>{
-        conn.query(`select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
-        from pregunta
-        left join etiqueta_pregunta
-        on pregunta.id =  etiqueta_pregunta.id_pregunta
-        left join etiqueta
-        on etiqueta_pregunta.id_etiqueta = etiqueta.id
-        where titulo LIKE ?
-        group by pregunta.id
-        order by id desc
-        limit 10 offset ?;`, [dynamicInput, offset] ,(err, lista_preguntas)=>{
-
-            callback(err, lista_preguntas)
-
-        });
-
-    });
-}
-
-function buscar_no_respondidas(req, dynamicInput, offset, callback){
-
-    req.getConnection((err, conn)=>{
-
-        conn.query(`select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
-        from (
-            select pregunta.*, COUNT(respuesta.id) as num_respuestas
-            from pregunta
-            left join respuesta
-            on pregunta.id = respuesta.idPregunta
-            group by pregunta.id
-        ) as pregunta
-        left join etiqueta_pregunta
-        on pregunta.id =  etiqueta_pregunta.id_pregunta
-        left join etiqueta
-        on etiqueta_pregunta.id_etiqueta = etiqueta.id
-        where titulo LIKE ? and pregunta.num_respuestas = 0
-        group by pregunta.id
-        order by id desc
-        limit 10 offset ?;`, [dynamicInput, offset] ,(err, lista_preguntas)=>{
-
-            callback(err, lista_preguntas)
-
-        });
-
-    });
-}
-
-function buscar_respondidas(req, dynamicInput, offset, callback){
-
-    req.getConnection((err, conn)=>{
-        
-        conn.query(`select pregunta.*, ifnull(GROUP_CONCAT(etiqueta.nombre), '') as etiquetas
-        from (
-            select pregunta.*, COUNT(respuesta.id) as num_respuestas
-            from pregunta
-            left join respuesta
-            on pregunta.id = respuesta.idPregunta
-            group by pregunta.id
-        ) as pregunta
-        left join etiqueta_pregunta
-        on pregunta.id =  etiqueta_pregunta.id_pregunta
-        left join etiqueta
-        on etiqueta_pregunta.id_etiqueta = etiqueta.id
-        where titulo LIKE ? and pregunta.num_respuestas > 0
-        group by pregunta.id
-        order by id desc
-        limit 10 offset ?;`, [dynamicInput, offset] ,(err, lista_preguntas)=>{
-
-            callback(err, lista_preguntas)
-
-        });
-
-    });
-}
 
 
 
